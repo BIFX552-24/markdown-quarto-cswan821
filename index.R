574ecec8d11438686b1564693eea3bde4850181d
cswan821 <cswann821@hotmail.com>
2024-08-20 19:38:25 -0400

    Add files via upload
    assignment 1 redo- still needs dark mode, turned to word doc

---
+++ index.R
@@ -0,0 +1,7 @@
+echo "# markdown-quarto-cswan821" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BIFX552-24/markdown-quarto-cswan821.git
+git push -u origin main
